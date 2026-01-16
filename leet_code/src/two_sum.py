561f2fd3e00ae1d4cf3c54e11819a28745da5c7e
vamsikrishna.rompicherla <vamsikrishna290@gmail.com>
2026-01-16 10:49:18 -0800

    two sum updates using dict approach for optimization

--- leet_code/src/two_sum.py
+++ leet_code/src/two_sum.py
@@ -4,16 +4,26 @@ from typing import List
 class Solution:
     @staticmethod
     def two_sum(nums: List[int], target: int) -> List[int]:
+        arr = []
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
-                    return [i,j]
+                    arr.append([i,j])
 
+        return arr
+
+    @staticmethod
+    def two_sum_with_dict(nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
         return []
 
 
 if __name__ == '__main__':
     sol = Solution()
-    print(sol.two_sum([2,7,11,15], 9))
-
-
+    print(sol.two_sum([2,7,11,15, 6, 3], 9))
+    print(sol.two_sum_with_dict([2,7,11,15, 6, 3], 9))
